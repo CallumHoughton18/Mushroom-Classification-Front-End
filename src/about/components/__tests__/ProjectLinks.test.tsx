57282941221838402a7ce655ad7686ec901a673e
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-07-22 18:32:54 +0100

    Added jest config to package.json, Added initial ProjectLinks snapshot tests

---
+++ src/about/components/__tests__/ProjectLinks.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {render} from "@testing-library/react";
+
+import ProjectLinks from "../ProjectLinks";
+
+describe("<ProjectLinks /> render test", () => {
+    test("should display Github and Docker project links", () => {
+        const renderedLinks = render(
+            <ProjectLinks PrimaryLogoColor="green" SecondaryLogoColor="blue" style="" />
+        );
+        expect(renderedLinks).toMatchSnapshot();
+    });
+});
