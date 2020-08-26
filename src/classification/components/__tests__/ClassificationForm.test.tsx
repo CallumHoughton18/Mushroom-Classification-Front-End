929b50f06025ab9eff471035a4df092f408f7dcc
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-08-26 15:12:30 +0100

    removed unneeded todos from ClassificationForm.test.tsx

--- src/classification/components/__tests__/ClassificationForm.test.tsx
+++ src/classification/components/__tests__/ClassificationForm.test.tsx
@@ -1,6 +1,5 @@
 import {render} from "@testing-library/react";
 import {generateClassificationFormSUT} from "../../../test_helpers/classificationTestHelpers";
-//TODO: Need to wrap some rendering in act()
 
 // These are purely unit tests, so shouldn't be dependent on internal hook implementation
 // but it would be overdoing it to mock the useForm hook as it shouldn't lead to a flakey test
@@ -12,5 +11,3 @@ describe("<ClassificationForm /> render test", () => {
         expect(renderedClassificationForm).toMatchSnapshot();
     });
 });
-
-//TODO: add test for when form definition is null
